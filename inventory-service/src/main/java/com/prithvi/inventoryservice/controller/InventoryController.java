495134db43c834ea908c3f2ca4b0172aa2e03d5e
itsprithvi <prithvi.myk@gmail.com>
2023-09-12 16:39:49 +0530

    order items

--- inventory-service/src/main/java/com/prithvi/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/prithvi/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.prithvi.inventoryservice.controller;
 
+import com.prithvi.inventoryservice.dto.InventoryResponse;
 import com.prithvi.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,9 +15,13 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // http://localhost:8082/api/inventory/iphone-15
+    // http://localhost:8082/api/inventory?skuCode=iphone-15&skuCode=iphone15-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
+
+
 }
--- order-service/src/main/java/com/prithvi/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/prithvi/orderservice/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.prithvi.orderservice.service;
 
 
+import com.prithvi.orderservice.dto.InventoryResponse;
 import com.prithvi.orderservice.dto.OrderLineItemsDto;
 import com.prithvi.orderservice.dto.OrderRequest;
 import com.prithvi.orderservice.models.Order;
@@ -9,6 +10,7 @@ import com.prithvi.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.*;
 
@@ -18,7 +20,10 @@ import java.util.*;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest) {
+        System.out.println("Order creating....");
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -26,6 +31,24 @@ public class OrderService {
                 .map(this::mapToDto).toList();
 
         order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        // Call Inventory service , and place if product is in stock
+        InventoryResponse[] inventoryResponsesArray = webClient.get().uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                        .retrieve()
+                                .bodyToMono(InventoryResponse[].class)
+                                        .block();
+
+        boolean allProductsInStock =  Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
         orderRepository.save(order);
     }
 
